3f821d65a6ff963b00c2416f4e9ada75c22f61f3
jarsbe <jarsbe@gmail.com>
2015-05-31 14:25:03 +0100

    Added actions tests.

---
+++ __tests__/stores/books-actions-tests.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import BooksActions from '../../src/actions/books-actions';
+import BooksStore from '../../src/stores/books-store';
+import BooksWebUtils from '../../src/web-utils/books-web-utils';
+import TestUtils from 'worsley/test-utils';
+import assert from 'assert';
+import sinon from 'sinon';
+
+describe('BooksActions', () => {
+  const book = { id: 1, title: 'Test Title' };
+  let booksActions;
+  let booksStore;
+
+  beforeEach(() => {
+    booksActions = new BooksActions(TestUtils.worsley);
+    booksStore = new BooksStore(TestUtils.worsley, booksActions);
+  });
+
+  describe('#addBook', () => {
+
+    it('adds the payload to registered stores', () => {
+      booksActions.addBook(book);
+      assert.deepEqual(booksStore.state.books, [book]);
+    });
+  });
+
+  describe('#beginLoadingBooks', () => {
+    let stub;
+
+    beforeEach(() => {
+      stub = sinon.stub(BooksWebUtils, 'fetch');
+    });
+
+    it('calls webutils', () => {
+      booksActions.beginLoadingBooks();
+      sinon.assert.calledOnce(stub);
+    });
+
+    afterEach(() => {
+      BooksWebUtils.fetch.restore();
+    });
+  });
+});
